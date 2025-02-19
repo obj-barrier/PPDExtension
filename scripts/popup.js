65ea93ff87bd01949754d632ffa74dc5a6912fbf
Obj. Barrier <obj_barrier@163.com>
2025-02-19 17:39:56 -0600

    Fix Login Issue

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,6 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let user_id = null;
+    const tabs = document.querySelectorAll(".tab");
+    const contents = document.querySelectorAll(".tab-content");
+    function showTab(tabId) {
+        tabs.forEach(tab => tab.classList.remove("active"));
+        contents.forEach(content => content.classList.remove("active"));
+        document.querySelector(`.tab[data-tab="${tabId}"]`).classList.add("active");
+        document.getElementById(`tab-${tabId}`).classList.add("active");
+    }
+    tabs.forEach(tab => {
+        tab.addEventListener("click", function () {
+            showTab(this.dataset.tab);
+        });
+    });
 
+    let page_text = null;
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, { action: "getPageText" }, (response) => {
+            if (chrome.runtime.lastError || !response) {
+                return;
+            } else {
+                page_text = response.text;
+            }
+        });
+    });
+
+    let user_id = null;
     document.getElementById("user1").addEventListener("click", function () {
         user_id = 1;
         login();
@@ -14,96 +38,66 @@ document.addEventListener("DOMContentLoaded", function () {
         login();
     });
 
-    const statusCode = "Status Code: "
-    const response = "Response: "
+    const statusHead = "Status Code: "
+    const responseHead = "Response: "
     const welcomeMsg = "Hi! I'm your personal shopping assistant.\nPlease tell me about your usage of the product so I can better assist you."
     // const url = "http://127.0.0.1:5000/";
     const url = "https://objbarrier.pythonanywhere.com/";
+
     const chatBox = document.getElementById("chat-box");
+    const status = document.getElementById("status");
+    const response = document.getElementById("response");
 
     let session_id = null;
     let history = [{ role: "bot", message: welcomeMsg }];
+    async function login() {
+        const stored = await chrome.storage.local.get(["user_id", "user_name", "session_id", "history"]);
+        let user_name = stored.user_name;
+        session_id = stored.session_id;
+        if (!stored.user_id || stored.user_id !== user_id) {
+            chrome.storage.local.clear();
+            chrome.storage.local.set({ "user_id": user_id, "history": history });
+            session_id = null;
 
-    function login() {
+            await fetch(url + "api/users/" + user_id, {
+                method: "GET",
+            })
+                .then(response => {
+                    status.textContent = statusHead + response.status;
+                    return response.json();
+                })
+                .then(data => {
+                    response.textContent = responseHead + JSON.stringify(data);
+                    user_name = data.name;
+                    chrome.storage.local.set({ "user_name": data.name });
+                })
+                .catch(error => {
+                    status.textContent = statusHead + "Error";
+                    response.textContent = responseHead + error.message;
+                });
+        }
         document.getElementById("login").style.display = "none";
-        document.getElementById("welcome").style.display = "block";
-        
-        chrome.storage.local.get(["user_id", "session_id", "history"], function (data) {
-            if (data.user_id) {
-                if (data.user_id !== user_id) {
-                    chrome.storage.local.clear();
-                    chrome.storage.local.set({ "user_id": user_id });
-                    chrome.storage.local.set({ "history": history });
-                    return;
-                }
-            } else {
-                chrome.storage.local.set({ "user_id": user_id });
-            }
 
-            if (data.history) {
-                history = data.history;
+        if (session_id) {
+            showTab("1");
+            document.getElementById("main").style.display = "block";
+            if (stored.history) {
+                history = stored.history;
             } else {
                 chrome.storage.local.set({ "history": history });
             }
+        } else {
+            document.getElementById("wel-label").textContent = "Welcome, " + user_name + "!";
+            document.getElementById("welcome").style.display = "block";
+        }
 
-            if (data.session_id) {
-                session_id = data.session_id;
-                showTab("1");
-                document.getElementById("welcome").style.display = "none";
-                document.getElementById("main").style.display = "block";
-                chatBox.innerHTML = data.history.map(msg => {
-                    const message = document.createElement("div");
-                    message.classList.add("message", msg.role);
-                    message.innerHTML = msg.message;
-                    return message.outerHTML;
-                }).join("");
-            } else {
-                fetch(url + "api/users/" + user_id, {
-                    method: "GET",
-                })
-                    .then(response => {
-                        document.getElementById("status").textContent = `${statusCode}${response.status}`;
-                        return response.json();
-                    })
-                    .then(data => {
-                        document.getElementById("response").textContent = `${response}${data}`;
-                        document.getElementById("welcome-name").textContent = "Welcome, " + data.name + "!";
-                    })
-                    .catch(error => {
-                        document.getElementById("status").textContent = `${statusCode}Error`;
-                        document.getElementById("response").textContent = `${response}${error.message}`;
-                    });
-            }
-        });
+        chatBox.innerHTML = history.map(msg => {
+            const message = document.createElement("div");
+            message.classList.add("message", msg.role);
+            message.innerHTML = msg.message;
+            return message.outerHTML;
+        }).join("");
     }
-
-    let page_text = null;
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, { action: "getPageText" }, (response) => {
-            if (chrome.runtime.lastError || !response) {
-                return;
-            } else {
-                page_text = response.text;
-            }
-        });
-    });
-
-    const tabs = document.querySelectorAll(".tab");
-    const contents = document.querySelectorAll(".tab-content");
-    showTab("1");
-
-    function showTab(tabId) {
-        tabs.forEach(tab => tab.classList.remove("active"));
-        contents.forEach(content => content.classList.remove("active"));
-        document.querySelector(`.tab[data-tab="${tabId}"]`).classList.add("active");
-        document.getElementById(`tab-${tabId}`).classList.add("active");
-    }
-
-    tabs.forEach(tab => {
-        tab.addEventListener("click", function () {
-            showTab(this.dataset.tab);
-        });
-    });
 
     document.getElementById("create-session").addEventListener("click", function () {
         const api = url + "api/users/" + user_id + "/shopping_sessions";
@@ -116,21 +110,20 @@ document.addEventListener("DOMContentLoaded", function () {
             body: JSON.stringify(data),
         })
             .then(response => {
-                document.getElementById("status").textContent = `${statusCode}${response.status}`;
+                status.textContent = statusHead + response.status;
                 return response.json();
             })
             .then(data => {
                 session_id = data.session_id;
                 chrome.storage.local.set({ "session_id": session_id });
-                // console.log("session_id: " + session_id);
                 showTab("1");
                 document.getElementById("welcome").style.display = "none";
                 document.getElementById("main").style.display = "block";
-                document.getElementById("response").textContent = `${response}${data}`;
+                response.textContent = responseHead + JSON.stringify(data);
             })
             .catch(error => {
-                document.getElementById("status").textContent = `${statusCode}Error`;
-                document.getElementById("response").textContent = `${response}${error.message}`;
+                status.textContent = statusHead + "Error";
+                response.textContent = responseHead + error.message;
             });
     });
 
@@ -167,11 +160,11 @@ document.addEventListener("DOMContentLoaded", function () {
         })
             .then(response => {
                 document.getElementById("send-btn").textContent = "Send";
-                document.getElementById("status").textContent = `${statusCode}${response.status}`;
+                status.textContent = statusHead + response.status;
                 return response.json();
             })
             .then(data => {
-                document.getElementById("response").textContent = `${response}${data}`;
+                response.textContent = responseHead + Object.keys(data[0]) + " " + data[0].created_at;
                 const botMessage = document.createElement("div");
                 botMessage.classList.add("message", "bot");
                 botMessage.innerHTML = marked.parse(data[0].content);
@@ -182,8 +175,8 @@ document.addEventListener("DOMContentLoaded", function () {
                 chatBox.scrollTop = chatBox.scrollHeight;
             })
             .catch(error => {
-                document.getElementById("status").textContent = `${statusCode}Error`;
-                document.getElementById("response").textContent = `${response}${error.message}`;
+                status.textContent = statusHead + "Error";
+                response.textContent = responseHead + error.message;
             });
     }
 
@@ -199,25 +192,25 @@ document.addEventListener("DOMContentLoaded", function () {
             body: JSON.stringify(data),
         })
             .then(response => {
-                document.getElementById("status").textContent = `${statusCode}${response.status}`;
-                // document.getElementById("gen-btn").textContent = "Start Generating";
+                status.textContent = statusHead + response.status;
+                document.getElementById("gen-btn").textContent = "Start Generating";
                 return response.json();
             })
             .then(data => {
-                document.getElementById("response").textContent = `${response}${data}`;
+                response.textContent = responseHead + Object.keys(data[0]) + " " + data[0].created_at;
                 document.getElementById("description").innerHTML = marked.parse(data[0].content);
                 showTab("2");
             })
             .catch(error => {
-                document.getElementById("status").textContent = `${statusCode}Error`;
-                document.getElementById("response").textContent = `${response}${error.message}`;
+                status.textContent = statusHead + "Error";
+                response.textContent = responseHead + error.message;
             });
     });
 
     document.getElementById("reset-btn").addEventListener("click", function () {
         session_id = null;
-        history = [{ role: "bot", message: welcomeMsg }];
         chrome.storage.local.remove("session_id");
+        history = [{ role: "bot", message: welcomeMsg }];
         chrome.storage.local.set({ "history": history });
         document.getElementById("welcome").style.display = "block";
         document.getElementById("main").style.display = "none";
