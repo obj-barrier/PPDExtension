465010a4374599d84330972096260c8b58b2a8b6
Obj. Barrier <obj_barrier@163.com>
2025-02-27 13:05:41 -0600

    Strings and HTML Replacing

--- scripts/content.js
+++ scripts/content.js
@@ -1,22 +1,23 @@
 const prodID = window.location.href.match(/dp\/([^\/?]*)/);
 if (prodID) {
     const prodIDKey = `desc_${prodID[1]}`;
-    chrome.storage.local.get(["user_id", "session_id", prodIDKey], async function (stored) {
+    chrome.storage.local.get(['user_id', 'session_id', prodIDKey], async function (stored) {
         if (!stored.user_id || !stored.session_id) {
             return;
         }
 
-        const genMessage = document.createElement("div");
-        genMessage.style.color = "red";
-        const regenBtn = document.createElement("button");
-        regenBtn.textContent = "Regenerate";
-        regenBtn.addEventListener("click", generate);
-        const title = document.getElementById("title");
-        title.appendChild(genMessage);
-        title.appendChild(regenBtn);
+        const genMessage = document.createElement('h3');
+        genMessage.style.color = 'red';
+        const regenBtn = document.createElement('button');
+        regenBtn.textContent = 'Regenerate';
+        regenBtn.addEventListener('click', generate);
+        document.getElementById('nile-inline_feature_div').remove();
+        const leftCol = document.getElementById('leftCol');
+        leftCol.appendChild(genMessage);
+        leftCol.appendChild(regenBtn);
 
         if (stored[prodIDKey]) {
-            genMessage.textContent = "Already has description!";
+            genMessage.textContent = 'Already has description!';
             return;
         }
         generate();
@@ -24,13 +25,13 @@ if (prodID) {
         function generate() {
             regenBtn.disabled = true;
             chrome.storage.local.remove(prodIDKey);
-            genMessage.textContent = "Generating product description...";
+            genMessage.textContent = 'Generating product description...';
 
-            // const api = "http://127.0.0.1:5000/api/";
-            const api = "https://dk1414.pythonanywhere.com/api/";
+            // const api = 'http://127.0.0.1:5000/api/';
+            const api = 'https://dk1414.pythonanywhere.com/api/';
             fetch(`${api}shopping_sessions/${stored.session_id}/product_description`, {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ product_page: document.body.innerText }),
             })
                 .then(response => {
@@ -38,7 +39,7 @@ if (prodID) {
                 })
                 .then(data => {
                     chrome.storage.local.set({ [prodIDKey]: data[0].content });
-                    genMessage.textContent = "Generation complete!";
+                    genMessage.textContent = 'Generation complete!';
                     regenBtn.disabled = false;
                 })
                 .catch(error => {
--- scripts/popup.js
+++ scripts/popup.js
@@ -20,68 +20,68 @@ function jsonToHtmlTable(json) {
     return html;
 }
 
-document.addEventListener("DOMContentLoaded", async function () {
-    const tabs = document.querySelectorAll(".tab");
-    const contents = document.querySelectorAll(".tab-content");
+document.addEventListener('DOMContentLoaded', async function () {
+    const tabs = document.querySelectorAll('.tab');
+    const contents = document.querySelectorAll('.tab-content');
     function showTab(tabId) {
-        tabs.forEach(tab => tab.classList.remove("active"));
-        contents.forEach(content => content.classList.remove("active"));
-        document.querySelector(`.tab[data-tab="${tabId}"]`).classList.add("active");
-        document.getElementById(`tab-${tabId}`).classList.add("active");
+        tabs.forEach(tab => tab.classList.remove('active'));
+        contents.forEach(content => content.classList.remove('active'));
+        document.querySelector(`.tab[data-tab='${tabId}']`).classList.add('active');
+        document.getElementById(`tab-${tabId}`).classList.add('active');
     }
     tabs.forEach(tab => {
-        tab.addEventListener("click", function () {
+        tab.addEventListener('click', function () {
             showTab(this.dataset.tab);
         });
     });
 
-    const statusHead = "Status Code: ";
-    const responseHead = "Response: ";
+    const statusHead = 'Status Code: ';
+    const responseHead = 'Response: ';
     const welcomeMsg = "Hi! I'm your personal shopping assistant.\nPlease tell me about your usage of the product so I can better assist you.";
-    // const api = "http://127.0.0.1:5000/api/";
-    const api = "https://dk1414.pythonanywhere.com/api/";
+    // const api = 'http://127.0.0.1:5000/api/';
+    const api = 'https://dk1414.pythonanywhere.com/api/';
 
-    const loginPanel = document.getElementById("login");
-    const welPanel = document.getElementById("welcome");
-    const welLabel = document.getElementById("wel-label");
-    const mainPanel = document.getElementById("main");
-    const chatBox = document.getElementById("chat-box");
-    const descBox = document.getElementById("description");
-    const compBox = document.getElementById("comparison");
-    const compBtn = document.getElementById("comp-btn");
-    const statusLabel = document.getElementById("status");
-    const responseLabel = document.getElementById("response");
+    const loginPanel = document.getElementById('login');
+    const welPanel = document.getElementById('welcome');
+    const welLabel = document.getElementById('wel-label');
+    const mainPanel = document.getElementById('main');
+    const chatBox = document.getElementById('chat-box');
+    const descBox = document.getElementById('description');
+    const compBox = document.getElementById('comparison');
+    const compBtn = document.getElementById('comp-btn');
+    const statusLabel = document.getElementById('status');
+    const responseLabel = document.getElementById('response');
 
     // await chrome.storage.local.clear();
-    // await chrome.storage.local.set({ "user_id": -1, "full_name": "Test", "session_id" : -1, "history": [{ role: "bot", message: welcomeMsg }] });
-    const stored = await chrome.storage.local.get(["user_id", "full_name", "session_id", "history", ]);
+    // await chrome.storage.local.set({ 'user_id': -1, 'full_name': 'Test', 'session_id' : -1, 'history': [{ role: 'bot', message: welcomeMsg }] });
+    const stored = await chrome.storage.local.get(['user_id', 'full_name', 'session_id', 'history', 'comparison' ]);
     let user_id = stored.user_id;
     let full_name = stored.full_name;
     let session_id = null;
-    let history = [{ role: "bot", message: welcomeMsg }];
+    let history = [{ role: 'bot', message: welcomeMsg }];
 
     function loadHistory() {
         chatBox.innerHTML = history.map(msg => {
-            const message = document.createElement("div");
-            message.classList.add("message", msg.role);
+            const message = document.createElement('div');
+            message.classList.add('message', msg.role);
             message.innerHTML = msg.message;
             return message.outerHTML;
-        }).join("");
+        }).join('');
     }
 
     if (user_id) {
-        loginPanel.style.display = "none";
+        loginPanel.style.display = 'none';
         welLabel.textContent = `Welcome, ${full_name} !`;
 
         if (stored.session_id) {
             session_id = stored.session_id;
             compBtn.disabled = false;
-            showTab("1");
-            mainPanel.style.display = "block";
+            showTab('1');
+            mainPanel.style.display = 'block';
             if (stored.history) {
                 history = stored.history;
             } else {
-                chrome.storage.local.set({ "history": history });
+                chrome.storage.local.set({ 'history': history });
             }
 
             chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
@@ -91,29 +91,33 @@ document.addEventListener("DOMContentLoaded", async function () {
                     chrome.storage.local.get(prodIDKey, function (stored_desc) {
                         if (stored_desc) {
                             descBox.innerHTML = marked.parse(stored_desc[prodIDKey]);
-                            showTab("2");
+                            showTab('2');
                         }
                     });
                 }
             });
+
+            if (stored.comparison) {
+                compBox.innerHTML = jsonToHtmlTable(stored.comparison);
+            }
         } else {
             compBtn.disabled = true;
-            welPanel.style.display = "block";
+            welPanel.style.display = 'block';
         }
 
         loadHistory();
     }
 
-    document.getElementById("login-btn").addEventListener("click", function () {
-        const email = document.getElementById("email").value.trim();
-        const password = document.getElementById("password").value;
-        if (email === "" || password === "") {
+    document.getElementById('login-btn').addEventListener('click', function () {
+        const email = document.getElementById('email').value.trim();
+        const password = document.getElementById('password').value;
+        if (email === '' || password === '') {
             return;
         }
 
         fetch(`${api}login`, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
                 email: email,
                 password: password
@@ -130,9 +134,9 @@ document.addEventListener("DOMContentLoaded", async function () {
                 responseLabel.textContent = `${responseHead}${JSON.stringify(data)}`;
                 user_id = data.user_id;
                 full_name = data.name;
-                chrome.storage.local.set({ "user_id": user_id, "full_name": full_name });
-                loginPanel.style.display = "none";
-                welPanel.style.display = "block";
+                chrome.storage.local.set({ 'user_id': user_id, 'full_name': full_name });
+                loginPanel.style.display = 'none';
+                welPanel.style.display = 'block';
                 welLabel.textContent = `Welcome, ${full_name} !`;
             })
             .catch(error => {
@@ -141,29 +145,29 @@ document.addEventListener("DOMContentLoaded", async function () {
             });
     });
 
-    const regBtn = document.getElementById("reg-btn");
-    regBtn.addEventListener("click", function () {
-        regBtn.style.display = "none";
-        document.getElementById("register").style.display = "block";
+    const regBtn = document.getElementById('reg-btn');
+    regBtn.addEventListener('click', function () {
+        regBtn.style.display = 'none';
+        document.getElementById('register').style.display = 'block';
     });
 
-    document.getElementById("submit-btn").addEventListener("click", function () {
-        const email = document.getElementById("email").value.trim();
-        const password = document.getElementById("password").value;
-        if (password !== document.getElementById("confirm-pwd").value) {
+    document.getElementById('submit-btn').addEventListener('click', function () {
+        const email = document.getElementById('email').value.trim();
+        const password = document.getElementById('password').value;
+        if (password !== document.getElementById('confirm-pwd').value) {
             return;
         }
-        const inputName = document.getElementById("full-name").value.trim();
-        const birthday = document.getElementById("birthday").value;
-        const occupation = document.getElementById("occupation").value;
-        const zipcode = document.getElementById("zipcode").value;
-        if (email === "" || password === "" || inputName === "" || birthday === "" || occupation === "" || zipcode === "") {
+        const inputName = document.getElementById('full-name').value.trim();
+        const birthday = document.getElementById('birthday').value;
+        const occupation = document.getElementById('occupation').value;
+        const zipcode = document.getElementById('zipcode').value;
+        if (email === '' || password === '' || inputName === '' || birthday === '' || occupation === '' || zipcode === '') {
             return;
         }
 
         fetch(`${api}users`, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
                 name: inputName,
                 email: email,
@@ -178,19 +182,19 @@ document.addEventListener("DOMContentLoaded", async function () {
                 responseLabel.textContent = `${responseHead}${JSON.stringify(data)}`;
                 user_id = data.user_id;
                 full_name = data.name;
-                chrome.storage.local.set({ "user_id": user_id, "full_name": full_name });
-                loginPanel.style.display = "none";
-                welPanel.style.display = "block";
+                chrome.storage.local.set({ 'user_id': user_id, 'full_name': full_name });
+                loginPanel.style.display = 'none';
+                welPanel.style.display = 'block';
                 welLabel.textContent = `Welcome, ${full_name} !`;
 
                 return fetch(`${api}users/${user_id}/preferences`, {
-                    method: "POST",
-                    headers: { "Content-Type": "application/json" },
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({
                         preferences: [
-                            { key: "birthday", value: birthday },
-                            { key: "occupation", value: occupation },
-                            { key: "zipcode", value: zipcode }
+                            { key: 'birthday', value: birthday },
+                            { key: 'occupation', value: occupation },
+                            { key: 'zipcode', value: zipcode }
                         ]
                     })
                 });
@@ -210,15 +214,15 @@ document.addEventListener("DOMContentLoaded", async function () {
         
     });
 
-    document.getElementById("create-session").addEventListener("click", function () {
-        history = [{ role: "bot", message: welcomeMsg }];
-        chrome.storage.local.set({ "history": history });
+    document.getElementById('create-session').addEventListener('click', function () {
+        history = [{ role: 'bot', message: welcomeMsg }];
+        chrome.storage.local.set({ 'history': history });
         loadHistory();
 
         fetch(`${api}users/${user_id}/shopping_sessions`, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ intent: document.getElementById("intent").value.trim() }),
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ intent: document.getElementById('intent').value.trim() }),
         })
             .then(response => {
                 statusLabel.textContent = `${statusHead}${response.status}`;
@@ -227,11 +231,11 @@ document.addEventListener("DOMContentLoaded", async function () {
             .then(data => {
                 responseLabel.textContent = `${responseHead}${JSON.stringify(data)}`;
                 session_id = data.session_id;
-                chrome.storage.local.set({ "session_id": session_id });
+                chrome.storage.local.set({ 'session_id': session_id });
                 compBtn.disabled = false;
-                showTab("1");
-                welPanel.style.display = "none";
-                mainPanel.style.display = "block";
+                showTab('1');
+                welPanel.style.display = 'none';
+                mainPanel.style.display = 'block';
             })
             .catch(error => {
                 statusLabel.textContent = `${statusHead}Error`;
@@ -239,55 +243,55 @@ document.addEventListener("DOMContentLoaded", async function () {
             });
     });
 
-    document.getElementById("logout-btn").addEventListener("click", function () {
+    document.getElementById('logout-btn').addEventListener('click', function () {
         chrome.storage.local.clear();
         user_id = null;
         full_name = null;
         session_id = null;
-        history = [{ role: "bot", message: welcomeMsg }];
-        loginPanel.style.display = "block";
-        welPanel.style.display = "none";
-        mainPanel.style.display = "none";
+        history = [{ role: 'bot', message: welcomeMsg }];
+        loginPanel.style.display = 'block';
+        welPanel.style.display = 'none';
+        mainPanel.style.display = 'none';
     });
 
-    document.getElementById("send-btn").addEventListener("click", sendMessage);
-    document.getElementById("user-input").addEventListener("keypress", function (event) {
-        if (event.key === "Enter") {
+    document.getElementById('send-btn').addEventListener('click', sendMessage);
+    document.getElementById('user-input').addEventListener('keypress', function (event) {
+        if (event.key === 'Enter') {
             sendMessage();
         }
     });
 
     function sendMessage() {
-        document.getElementById("send-btn").textContent = "Sending...";
-        const inputField = document.getElementById("user-input");
+        document.getElementById('send-btn').textContent = 'Sending...';
+        const inputField = document.getElementById('user-input');
         const messageText = inputField.value.trim();
-        if (messageText === "") return;
+        if (messageText === '') return;
         
-        const userMessage = document.createElement("div");
-        userMessage.classList.add("message", "user");
+        const userMessage = document.createElement('div');
+        userMessage.classList.add('message', 'user');
         userMessage.textContent = messageText;
-        history.push({ role: "user", message: messageText });
-        chrome.storage.local.set({ "history": history });
+        history.push({ role: 'user', message: messageText });
+        chrome.storage.local.set({ 'history': history });
         chatBox.appendChild(userMessage);
 
-        inputField.value = "";
+        inputField.value = '';
         fetch(`${api}shopping_sessions/${session_id}/messages`, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ message: messageText }),
         })
             .then(response => {
-                document.getElementById("send-btn").textContent = "Send";
+                document.getElementById('send-btn').textContent = 'Send';
                 statusLabel.textContent = statusHead + response.status;
                 return response.json();
             })
             .then(data => {
                 responseLabel.textContent = `${responseHead}${Object.keys(data[0])} ${data[0].created_at}`;
-                const botMessage = document.createElement("div");
-                botMessage.classList.add("message", "bot");
+                const botMessage = document.createElement('div');
+                botMessage.classList.add('message', 'bot');
                 botMessage.innerHTML = marked.parse(data[0].content);
-                history.push({ role: "bot", message: botMessage.innerHTML });
-                chrome.storage.local.set({ "history": history });
+                history.push({ role: 'bot', message: botMessage.innerHTML });
+                chrome.storage.local.set({ 'history': history });
 
                 chatBox.appendChild(botMessage);
                 chatBox.scrollTop = chatBox.scrollHeight;
@@ -298,21 +302,21 @@ document.addEventListener("DOMContentLoaded", async function () {
             });
     }
 
-    document.getElementById("reset-btn").addEventListener("click", function () {
+    document.getElementById('reset-btn').addEventListener('click', function () {
         session_id = null;
         chrome.storage.local.clear();
-        history = [{ role: "bot", message: welcomeMsg }];
-        chrome.storage.local.set({ "user_id": user_id, "full_name": full_name, "history": history });
-        document.getElementById("welcome").style.display = "block";
-        mainPanel.style.display = "none";
+        history = [{ role: 'bot', message: welcomeMsg }];
+        chrome.storage.local.set({ 'user_id': user_id, 'full_name': full_name, 'history': history });
+        document.getElementById('welcome').style.display = 'block';
+        mainPanel.style.display = 'none';
     });
 
-    compBtn.addEventListener("click", function () {
+    compBtn.addEventListener('click', function () {
         compBtn.disabled = true;
-        compBtn.textContent = "Generating...";
+        compBtn.textContent = 'Generating...';
         fetch(`${api}shopping_sessions/${session_id}/product_comparison`, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({}),
         })
             .then(response => {
@@ -320,10 +324,11 @@ document.addEventListener("DOMContentLoaded", async function () {
                 return response.json();
             })
             .then(data => {
+                chrome.storage.local.set({ 'comparison': JSON.parse(data[0].content) });
                 responseLabel.textContent = `${responseHead}${Object.keys(data[0])}`;
                 compBox.textContent = data[0].content;
                 compBox.innerHTML = jsonToHtmlTable(JSON.parse(data[0].content));
-                compBtn.textContent = "Generate / Update Comparison";
+                compBtn.textContent = 'Generate / Update Comparison';
                 compBtn.disabled = false;
             })
             .catch(error => {
