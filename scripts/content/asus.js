5ad98ef7e8f4bbcc46c4c940c97391ec628df4e3
Obj. Barrier <obj_barrier@163.com>
2025-03-06 08:08:13 -0600

    Test Support for ASUS Website

---
+++ scripts/content/asus.js
@@ -0,0 +1,101 @@
+const prodID = window.location.href;
+if (prodID) {
+    let descBox = document.getElementsByClassName('product attribute overview')[0];
+
+    if (descBox) {
+        const origDesc = descBox.innerHTML;
+
+        const prodIDKey = `desc_${prodID}`;
+        chrome.storage.local.get(['user_id', 'session_id', prodIDKey], async function (stored) {
+            if (!stored.user_id || !stored.session_id) {
+                return;
+            }
+
+            let customDesc;
+
+            const genMessage = document.createElement('h3');
+            genMessage.style.color = 'red';
+
+            const regenBtn = document.createElement('button');
+            regenBtn.textContent = 'Regenerate';
+            regenBtn.addEventListener('click', generate);
+
+            const descSwitch = document.createElement('input');
+            descSwitch.type = 'checkbox';
+            descSwitch.style.marginLeft = '10px';
+            descSwitch.addEventListener('change', () => {
+                switchDesc(descSwitch.checked);
+            });
+
+            const descLabel = document.createElement('div');
+            descLabel.style.marginTop = '5px';
+            descLabel.textContent = 'Toggle Custom Description';
+            descLabel.appendChild(descSwitch);
+
+            const controlBox = document.createElement('div');
+            controlBox.style.textAlign = 'right';
+            controlBox.style.marginRight = '10px';
+            controlBox.appendChild(genMessage);
+            controlBox.appendChild(regenBtn);
+            controlBox.appendChild(descLabel);
+
+            document.getElementsByClassName('product media')[0].appendChild(controlBox);
+
+            if (stored[prodIDKey]) {
+                genMessage.textContent = 'Loaded existing description';
+                customDesc = marked.parse(stored[prodIDKey]);
+                switchDesc(true);
+                return;
+            }
+            generate();
+
+            function generate() {
+                regenBtn.disabled = true;
+                chrome.storage.local.remove(prodIDKey);
+                genMessage.textContent = 'Generating description...';
+
+                // const api = 'http://127.0.0.1:5000/api/';
+                const api = 'https://dk1414.pythonanywhere.com/api/';
+                fetch(`${api}shopping_sessions/${stored.session_id}/product_description`, {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ product_page: document.body.innerText }),
+                })
+                    .then(response => {
+                        return response.json();
+                    })
+                    .then(data => {
+                        chrome.storage.local.set({ [prodIDKey]: data[0].content });
+                        genMessage.textContent = 'Generating comparison...';
+                        customDesc = marked.parse(data[0].content);
+                        switchDesc(true);
+                        return fetch(`${api}shopping_sessions/${stored.session_id}/product_comparison`, {
+                            method: 'POST',
+                            headers: { 'Content-Type': 'application/json' },
+                            body: '{}'
+                        });
+                    })
+                    .then(response => response.json())
+                    .then(data => {
+                        chrome.storage.local.set({ 'comparison': JSON.parse(data[0].content) });
+                        genMessage.textContent = 'Generation complete!';
+                        regenBtn.disabled = false;
+                    })
+                    .catch(error => {
+                        console.error(error.message);
+                    });
+            }
+
+            function switchDesc(isCustom) {
+                descSwitch.checked = isCustom;
+                if (isCustom) {
+                    descBox.classList.add('pepper-box');
+                    descBox.innerHTML = customDesc;
+                } else {
+                    descBox.classList.remove('pepper-box');
+                    descBox.innerHTML = origDesc;
+                }
+            }
+        });
+    }
+}
